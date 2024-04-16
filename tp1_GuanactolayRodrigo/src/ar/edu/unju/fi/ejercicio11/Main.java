368b262cb7632e0ee9790d52c3168dc752cb1a71
Rodrigo <Rodrigo@DESKTOP-2SFTDIT>
2024-04-15 23:54:21 -0300

    Se agrega paquete y clase del ejercicio 11.

---
+++ tp1_GuanactolayRodrigo/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int j, i;
+		
+		for(j=40, i=2 ; j>0 ; j--,i++) {
+			System.out.print(j*i+" ");
+		}
+	}
+
+}
